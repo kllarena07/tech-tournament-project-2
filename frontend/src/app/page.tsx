5c4957362271db834a337681230f11f38f9c6274
Kieran Llarena <baymax709@gmail.com>
2025-01-23 18:02:56 -0500

    chore: created messaging ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,100 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Textarea } from "@/components/ui/textarea";
+import { ArrowBigUp } from "lucide-react";
 import Image from "next/image";
 
-export default function Home() {
+function UserMessage({ message }: { message: string }) {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <p className="max-w-[65%] bg-gray-100 px-5 py-2 rounded-lg mr-5">
+      {message}
+    </p>
+  );
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+function BotMessage({ message }: { message: string }) {
+  return (
+    <div className="flex items-start gap-2 ml-5">
+      <Image
+        src="/gemini-icon.png"
+        width={24}
+        height={24}
+        alt="Google Gemini"
+        className="shadow-[0_0_5px_rgba(0,0,0,0.5)] w-[24px] h-[24px] rounded-full bg-white flex-shrink-0"
+      />
+      <p className="w-full ml-2">{message}</p>
+    </div>
+  );
+}
+
+export default function Home() {
+  const message =
+    "The primary ethical concern for anthropologists is to conduct their research in a way that does not harm the people, animals, or artifacts they study. This includes respecting the rights, dignity, and well-being of those involved and ensuring that research practices are ethical and responsible.";
+
+  return (
+    <div className="flex flex-col h-dvh">
+      <header>
+        <h1 className="font-bold text-3xl text-center p-5">Gemini API Demo</h1>
+      </header>
+      <ScrollArea className="h-full">
+        {Array.from({ length: 50 }).map((_, index) => (
+          <div
+            className={`mb-5 flex w-full ${
+              index % 2 == 0 ? "justify-end" : "justify-start"
+            }`}
+            key={index}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+            {index % 2 == 0 ? (
+              <UserMessage message={message} />
+            ) : (
+              <BotMessage message={message} />
+            )}
+          </div>
+        ))}
+      </ScrollArea>
+      <footer className="flex px-5 pb-5 gap-5">
+        <Textarea placeholder="Message Gemini" className="resize-none" />
+        <Button className="h-full aspect-square">
+          <ArrowBigUp />
+        </Button>
       </footer>
     </div>
   );
